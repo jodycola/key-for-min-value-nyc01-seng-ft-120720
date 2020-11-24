ef1a5c088e9aacc5c95de5efb81662cc96d6e068
Kody Samaroo <kody.samaroo@gmail.com>
2020-11-24 17:29:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, val|
     if val > smallest
       smallest = val
-      answer = key[val]
+      answer = key
     end
   end
   return answer
