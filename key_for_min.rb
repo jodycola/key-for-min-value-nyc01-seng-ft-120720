912b0d5ae72ef922e944c20e70f9599d03ede2f7
Kody Samaroo <kody.samaroo@gmail.com>
2020-11-24 17:27:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest = 99
+  
+  name_hash.each do |key, val|
+    if val > smallest
+      smallest = val
+    end
+  end
+  return smallest
 end
